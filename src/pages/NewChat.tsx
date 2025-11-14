95651efb5f6879f5b568a72b3d7e278ff1e5a1bd
ujjwal105 <ujjwaltyagi2969@gmail.com>
2025-11-14 14:00:10 +0530

    add google api

--- src/pages/NewChat.tsx
+++ src/pages/NewChat.tsx
@@ -1,6 +1,18 @@
 import AI_Prompt from "@/components/kokonutui/ai-prompt";
+import { GoogleGenAI } from "@google/genai";
 
 function NewChat() {
+  const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+  async function api() {
+    const ai = new GoogleGenAI({ apiKey });
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents:
+        "write me a story with a hero and a dragon saving the realm. kel",
+    });
+    console.log(response.text);
+  }
+  api();
   return (
     <div className="flex flex-col items-center justify-center h-screen text-gray-300 font-sans relative dark:bg-neutral-900 bg-white rounded-lg border">
       <div className="flex flex-col items-center w-full max-w-4xl px-4">
